750a73cd8c910cd0d32dbd37acdf890db5d75fbb
Michal Dorner <dorner.michal@gmail.com>
2014-05-10 11:56:03 +0200

    add progress report to tf-idf

--- examples/tf-idf/src/tf_idf.erl
+++ examples/tf-idf/src/tf_idf.erl
@@ -42,7 +42,8 @@ start(RunID, Slaves, Args=[Src, Dest]) ->
             A+B
         end},
         % {output, edfs_lib:iter_write_list(100000, OutDir1, 1, fun erlang:term_to_binary/1)}
-        {output, edfs_lib:iter_write_list({size,"32MB"}, OutDir1, 1)}
+        {output, edfs_lib:iter_write_list({size,"32MB"}, OutDir1, 1)},
+        {progress, fun(P) -> io:format("done: ~p%~n",[P]) end }
     ]),
     erlduce_job:wait(Pid),
 
@@ -51,7 +52,7 @@ start(RunID, Slaves, Args=[Src, Dest]) ->
     {ok,Pid2} = erlduce_job:start_link(Slaves, [
         {id, {RunID,2}},
         {input, fun()-> edfs:input(OutDir1) end},
-        {map, fun(Path, Bytes, Write) ->
+        {map, fun(_Path, Bytes, Write) ->
             List = binary_to_term(Bytes),
             [ Write(DocID, {Word, WordCount}) ||  {{Word,DocID}, WordCount} <- List],
             ok
@@ -62,14 +63,15 @@ start(RunID, Slaves, Args=[Src, Dest]) ->
                 Write({{Word, DocID}, {WordCount, WordsInDoc}})
             end, Write0, Values)
         end},
-        {output, edfs_lib:iter_write_list({size,"32MB"}, OutDir2, 1)}
+        {output, edfs_lib:iter_write_list({size,"32MB"}, OutDir2, 1)},
+        {progress, fun(P) -> io:format("done: ~p%~n",[P]) end }
     ]),
     erlduce_job:wait(Pid2),
 
     {ok,Pid3} = erlduce_job:start_link(Slaves, [
         {id, {RunID,3}},
         {input, fun()-> edfs:input(OutDir2) end},
-        {map, fun(Path, Bytes, Write) ->
+        {map, fun(_Path, Bytes, Write) ->
             List = binary_to_term(Bytes),
             [ Write(Word, {DocID, WordCount, WordsInDoc}) ||  {{Word, DocID}, {WordCount, WordsInDoc}} <- List],
             ok
@@ -83,7 +85,8 @@ start(RunID, Slaves, Args=[Src, Dest]) ->
         % {output, fun edfs_lib:null_writter/1}
         {output, edfs_lib:iter_write_list({size,"32MB"}, Dest, 1, fun(Data)->
             [ [Word, "\t", DocID,"\t", float_to_list(TfIDF), "\n"] || {{Word, DocID}, TfIDF} <- Data]
-        end)}
+        end)},
+        {progress, fun(P) -> io:format("done: ~p%~n",[P]) end }
     ]),
     erlduce_job:wait(Pid3),
 
@@ -101,7 +104,7 @@ words_2(<<C, Rest/binary>>, Acc, Fun) when
     words_2(Rest, [C | Acc], Fun);
 words_2(<<_, Rest/binary>>, [], Fun) ->
     words_2(Rest, [], Fun);
-words_2(<<>>, [], Fun) ->
+words_2(<<>>, [], _Fun) ->
     ok;
 words_2(Rest, Acc, Fun) ->
     Word = list_to_binary(lists:reverse(Acc)),
